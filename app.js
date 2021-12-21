344c2e88754377dbd78b7e0b3831ad5fa6e0dc9c
Rahul-D78 <rahulparida933@gmail.com>
2021-12-21 15:40:03 +0530

    FEAT: Initial project setup
    Signed-off-by: Rahul-D78 <rahulparida933@gmail.com>

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express();
+
+const PORT = process.env.PORT || 3000
+
+app.get('/', (req, res) => {
+    res.send('HI')
+})
+
+app.listen(PORT, () => console.log(`server listening on port ${PORT}`))
