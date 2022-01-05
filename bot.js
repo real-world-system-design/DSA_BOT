6d96344a936daa5d8cc1677ea2e10b24844e55d5
Rahul-D78 <rahulparida933@gmail.com>
2022-01-05 23:38:32 +0530

    FEAT: Trapping the messge events on the console
    Signed-off-by: Rahul-D78 <rahulparida933@gmail.com>

--- bot.js
+++ bot.js
@@ -3,4 +3,13 @@ require('dotenv').config()
 const { Client } = require('discord.js');
 const bot = new Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
 
+bot.on('ready', () => {
+    console.log(`${bot.user.username} has logged in`);
+})
+
+//Message Event
+bot.on('messageCreate', (message) => {
+    console.log(message.content);
+})
+
 bot.login(process.env.DISCORDJS_BOT_TOKEN)
